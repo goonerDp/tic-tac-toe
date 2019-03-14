5f9aec15ce3085c8c6c53a269614dd884669fa74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-14 09:51:05 +0000

    Deploy goonerDp/tic-tac-toe to github.com/goonerDp/tic-tac-toe.git:gh-pages

---
+++ precache-manifest.790df37c192880179fca5c677e081364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef5b9b5853094c4cef1",
+    "url": "/tic-tac-toe/static/css/main.8f537976.chunk.css"
+  },
+  {
+    "revision": "cef5b9b5853094c4cef1",
+    "url": "/tic-tac-toe/static/js/main.0ad77e53.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "c5600b33a35fb7e80622",
+    "url": "/tic-tac-toe/static/js/2.2093480a.chunk.js"
+  },
+  {
+    "revision": "19244d07a8923b75a4b00fda30235c3f",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
